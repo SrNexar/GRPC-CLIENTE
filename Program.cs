8f7a3eb19954fdb935195d69a33ec88bd685bbaf
SrNexar <nfsalguero@uce.edu.ec>
2025-01-13 22:24:52 -0500

    First commit

---
+++ Program.cs
@@ -0,0 +1,7 @@
+﻿using GRCPSERVICE_C_;
+using Grpc.Net.Client;
+var channel = GrpcChannel.ForAddress("http://localhost:5291");
+var client = new Greeter.GreeterClient(channel);
+
+var reply = client.SayHello(new HelloRequest { Name = "World" });
+Console.WriteLine("Greeting: " + reply.Message);
